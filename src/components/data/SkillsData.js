1147312abbfa908c7e3c7671f3fec3107cc7d553
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-19 12:42:13 +0530

    skill component done

---
+++ src/components/data/SkillsData.js
@@ -0,0 +1,36 @@
+export const SkillsData = [
+  {
+    type: "Frontend",
+    list: [
+      {
+        name: "ReactJS",
+        icon: <i className="devicon-react-original colored"></i>,
+      },
+      {
+        name: "CSS3",
+        icon: <i className="devicon-css3-plain colored"></i>,
+      },
+      {
+        name: "JavaScript",
+        icon: <i className="devicon-javascript-plain colored"></i>,
+      },
+      {
+        name: "HTML5",
+        icon: <i className="devicon-html5-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Backend",
+    list: [
+      {
+        name: "Node.js",
+        icon: <i className="devicon-nodejs-plain colored"></i>,
+      },
+      {
+        name: "C++",
+        icon: <i className="devicon-cplusplus-plain colored"></i>,
+      },
+    ],
+  },
+];
